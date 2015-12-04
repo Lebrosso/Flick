eb2f09d836b546648298c9b8e455a31f4a18d035
jacek <jacsapie@gmail.com>
2015-12-04 15:55:19 +0100

    test

---
+++ WebContent/js/js.js
@@ -0,0 +1,30 @@
+angular.module('photoApp',['ngRoute']).config(function($httpProvider){
+	  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	});
+	
+	
+	
+var app = angular.module('imageViewer', ['ng', 'ngResource','ngSanitize']);
+
+app.controller('AppGallery',[ '$scope','$http', function AppGallery($scope, $http) {
+$http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json').success(function (data) {
+  
+});
+	
+	jsonFlickrFeed = function(data){
+	$scope.data = data.items;
+
+	}
+}]);
+
+angular.module('scroll', []).directive('whenScrolled', function() {
+    return function(scope, elm, attr) {
+        var raw = elm[0];
+        
+        elm.bind('scroll', function() {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
